8c34e59b0cd2359d3b3fd3e3f483d518d4650960
Maxime Aoustin <670701+Maxwell2022@users.noreply.github.com>
2024-10-25 14:42:29 +1100

    init

---
+++ packages/b/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: "swapi-graphql",
+      url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+    },
+    includes: ["*.graphql"],
+  },
+};
